785b19f0ac8be9e13ed1a12c25499f1e57e4b89d
Lukman Bioku <40294520+sholabioku@users.noreply.github.com>
2021-09-17 08:33:01 +0000

    Another way of solving it

--- javascript_cardio/capitalizeletters.js
+++ javascript_cardio/capitalizeletters.js
@@ -14,12 +14,23 @@ function capitalizeLetters(str) {
   //   return char.toUpperCase();
   // })
 
-  const words = [];
-  for (let word of str.split(' ')) {
-    words.push(word[0].toUpperCase() + word.slice(1))
-  }
+  // const words = [];
+  // for (let word of str.split(' ')) {
+  //   words.push(word[0].toUpperCase() + word.slice(1))
+  // }
 
-  return words.join(' ')
+  // return words.join(' ')
+
+  let result = str[0].toUpperCase();
+
+  for(let i = 1; i < str.length; i++) {
+    if(str[i - 1] === ' '){
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i]
+    }
+  }
+  return result;
 
 }
 
